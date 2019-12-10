6e5f41755563da7d078b963a8e66541073c4b112
nalin <stepupithub@gmail.com>
2019-12-10 16:40:29 +0530

    Fixed update profile issue

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -201,46 +201,29 @@ exports.expRegister = function(req, res) {
 
 exports.expregUpdate = function(req, res) {
     var userModel = mongoose.model('expenseUser');
-    // userModel.find({
-    //     email: req.body.email
-    // }).exec(function(err, result) {
 
-    //     if (err) {
-    //         return;
-    //     }
-
-    //     if (result && result.length) {
-    //         res.json({
-    //             status: 1,
-    //         });
-
-    //         return;
-    //     }
-
-    //     req.body.createdAt = new Date();
-    //     var user = new userModel(req.body);
-
-    //     var errors = req.validationErrors();
-
-    //     if (errors) {
-    //         res.json({
-    //             status: 2,
-    //             errors: errors,
-    //         })
-    //         return
-    //     }
+    userModel.find({
+        email: req.body.email,
+        _id: { $ne: req.body._id }
+    }).exec(function(err, result) {
+        if (result && result.length) {
+            res.json({
+                status: 1,
+            });
+            return;
+        }
 
         userModel.update({
-        _id: req.body._id
+        	_id: req.body._id
 	    }, req.body, {
 	        multi: true
 	    }).exec(function(err, result) {
 	        res.json({
-	            status: true,
+	            status: 2,
 	            result: result
 	        });
 	    });
-    // });
+    });
 };
 
 exports.expLogin = function(req, res) {
